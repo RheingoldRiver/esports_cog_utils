e8267c1b568ebfcb23dd559d2a5a9063d8d76c37
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-02-07 04:42:56 -0600

    fix gitignore

---
+++ rivercogutils/red_site.py
@@ -0,0 +1,4 @@
+from esportswiki_editing import EsportsSite
+
+class RedSite(EsportsSite):
+    def __init__(self):
---
+++ rivercogutils/utils.py
@@ -0,0 +1,10 @@
+from esportswiki_editing import login
+
+async def login_if_possible(ctx, bot, wiki):
+    gamepedia_keys = await bot.get_shared_api_tokens("gamepedia")
+    if gamepedia_keys.get("account") is None:
+        await ctx.send("Sorry, you haven't set a Gamepedia bot account yet.")
+        return None
+    username = "{}@{}".format(gamepedia_keys.get("account"), gamepedia_keys.get("bot"))
+    password = gamepedia_keys.get("password")
+    return login('me', wiki, username=username, pwd=password)
