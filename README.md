9907de67df2a38c2aeadbc5c713e9fbe59505e94
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-02-07 04:42:18 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+Tools for River's Red cogs.
+
+To install:
+
+```pip install -U git+git://github.com/RheingoldRiver/rivercogutils```
---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="rivercogutils",
+    version="0.0.1",
+    author="RheingoldRiver",
+    author_email="river.esports@gmail.com",
+    description="River's Red cog tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/RheingoldRiver/esportswiki_editing",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.6',
+    install_requires=['mwclient', 'mwparserfromhell'],
+    dependency_links=['https://github.com/RheingoldRiver/esportswiki_editing#egg=0.1']
+)
