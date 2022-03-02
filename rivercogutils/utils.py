c9676eb0b40d69c66632b9aec8359ce534a12dc2
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2022-03-02 17:21:25 -0600

    separate method just to get credentials

--- rivercogutils/utils.py
+++ rivercogutils/utils.py
@@ -4,13 +4,19 @@ from mwrogue.esports_client import EsportsClient
 from mwrogue.auth_credentials import AuthCredentials
 
 
-async def login_if_possible(ctx, bot, wiki) -> Optional[EsportsClient]:
+async def get_credentials(ctx, bot) -> Optional[AuthCredentials]:
     gamepedia_keys = await bot.get_shared_api_tokens("gamepedia")
     if gamepedia_keys.get("account") is None:
         await ctx.send("Sorry, you haven't set a Gamepedia bot account yet.")
         return None
     username = "{}@{}".format(gamepedia_keys.get("account"), gamepedia_keys.get("bot"))
     password = gamepedia_keys.get("password")
-    credentials = AuthCredentials(username=username, password=password)
-    site = EsportsClient(wiki, credentials=credentials)
+    return AuthCredentials(username=username, password=password)
+
+
+async def login_if_possible(ctx, bot, wiki) -> Optional[EsportsClient]:
+    auth_credentials = await get_credentials(ctx, bot)
+    if auth_credentials is None:
+        return
+    site = EsportsClient(wiki, credentials=auth_credentials)
     return site
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="rivercogutils",
-    version="0.0.3",
+    version="0.0.4",
     author="RheingoldRiver",
     author_email="river.esports@gmail.com",
     description="River's Red cog tools",
