035cdd7caff157a160678bbe28c8f3ee567ffa50
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2022-04-24 20:53:19 -0500

    Rename repo

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="rivercogutils",
+    name="esports_cog_utils",
     version="0.0.5",
     author="RheingoldRiver",
     author_email="river.esports@gmail.com",
     description="River's Red cog tools",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/RheingoldRiver/rivercogutils",
+    url="https://github.com/RheingoldRiver/esports_cog_utils",
     packages=setuptools.find_packages(),
     python_requires='>=3.6',
     # install_requires=['mwclient', 'mwparserfromhell'],
-    dependency_links=['https://github.com/RheingoldRiver/river_mwclient#egg=0.1']
+    dependency_links=['mwrogue']
 )
