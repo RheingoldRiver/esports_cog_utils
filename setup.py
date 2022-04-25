b5148489f1a82bbda5eb2b9c03125d0d55ee637a
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2022-04-24 20:55:58 -0500

    reset version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="esports_cog_utils",
-    version="0.0.5",
+    version="0.0.1",
     author="RheingoldRiver",
     author_email="river.esports@gmail.com",
     description="River's Red cog tools",
